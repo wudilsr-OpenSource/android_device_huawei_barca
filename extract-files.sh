7a123f875b720b8355f5f2dc79ca27eea1b22f0e
Raphael Mounier <mounierr07@gmail.com>
2023-11-29 21:40:50 +0100

    prague: update to 416(C185) blobs

--- extract-files.sh
+++ extract-files.sh
@@ -10,20 +10,12 @@ function blob_fixup() {
     case "${1}" in
         odm/lib64/hwcam/hwcam.hi6250.m.PRAGUE.so)
             "${PATCHELF}" --add-needed "libtinyxml2_shim.so" "${2}"
-            # NOP out assertOk() and return_status()
-            "${SIGSCAN}" -p "e0 83 04 91 b8 52 f5 97" -P "e0 83 04 91 1f 20 03 d5" -f "${2}"
-            "${SIGSCAN}" -p "e0 83 04 91 b9 52 f5 97" -P "e0 83 04 91 1f 20 03 d5" -f "${2}"
             ;;
         vendor/etc/libnfc-brcm.conf)
             sed -i 's\/data/nfc\/data/vendor/nfc\g' "${2}"
             ;;
         vendor/etc/libnfc-nxp.conf)
             sed -i 's|libpn551_fw_10_05_03_64bits.so|libpn551_fw.so|g' "${2}"
-            ;;
-        vendor/etc/camera/*|odm/etc/camera/*)
-            sed -i 's/gb2312/iso-8859-1/g' "${2}"
-            sed -i 's/GB2312/iso-8859-1/g' "${2}"
-            sed -i 's/xmlversion/xml version/g' "${2}"
             ;;
     esac
 }
