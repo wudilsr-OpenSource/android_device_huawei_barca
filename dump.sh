af0eed7b72dfbd952c8e79402ed7644a18b2bab9
Raphael Mounier <mounierr07@gmail.com>
2023-12-28 18:00:14 +0100

    prague: add dump script to extract blob

---
+++ dump.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo ""
+echo "LineageOS 20.x dump blob for PRAGUE"
+echo "please put *.img in raw format here before start"
+echo "Executing in 5 seconds - CTRL-C to exit"
+echo ""
+sleep 5
+
+mkdir dump/system/
+mkdir dump/vendor/
+mkdir dump/product/
+mkdir dump/odm/
+
+sudo mount -o ro system.img dump/system/
+sudo mount -o ro vendor.img dump/vendor/
+sudo mount -o ro product.img dump/product/
+sudo mount -o ro odm.img dump/odm/
+
+echo "Start extraction"
+sleep 5
+sudo ./extract-files.sh prague ./dump/ > dump.log
+
+echo "Reset owner"
+sudo chown -R iceows:iceows ./../../../vendor/huawei/hi6250-8-common/*
+
+echo "Umount all"
+sleep 5
+sudo umount dump/system/
+sudo umount dump/vendor/
+sudo umount dump/product/
+sudo umount dump/odm/
+
+rm -rf dump/system/
+rm -rf dump/vendor/
+rm -rf dump/product/
+rm -rf dump/odm/
+
