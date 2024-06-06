4cc8b86c65a3864024f7819bc53f9462c4c50f23
Raphael Mounier <mounierr07@gmail.com>
2024-06-06 12:04:07 +0200

    prague: dump.sh add preavs img

--- dump.sh
+++ dump.sh
@@ -6,15 +6,18 @@ echo "Executing in 5 seconds - CTRL-C to exit"
 echo ""
 sleep 5
 
+mkdir dump
 mkdir dump/system/
 mkdir dump/vendor/
 mkdir dump/product/
 mkdir dump/odm/
+mkdir dump/preavs/
 
 sudo mount -o ro system.img dump/system/
 sudo mount -o ro vendor.img dump/vendor/
 sudo mount -o ro product.img dump/product/
 sudo mount -o ro odm.img dump/odm/
+sudo mount -o ro preavs.img dump/preavs/
 
 echo "Start extraction"
 sleep 5
@@ -30,9 +33,10 @@ sudo umount dump/system/
 sudo umount dump/vendor/
 sudo umount dump/product/
 sudo umount dump/odm/
+sudo umount dump/preavs/
 
 rm -rf dump/system/
 rm -rf dump/vendor/
 rm -rf dump/product/
 rm -rf dump/odm/
-
+rm -rf dump/preavs/
